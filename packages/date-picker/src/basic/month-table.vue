8dbfa4e8c7cf4528533482c2277dfcc9f4a3f4e1
youyudeqizhi <15558125545@163.com>
2022-06-14 10:27:01 +0800

    开发任务： 日期选择器

--- packages/date-picker/src/basic/month-table.vue
+++ packages/date-picker/src/basic/month-table.vue
@@ -1,5 +1,6 @@
 <template>
-  <table @click="handleMonthTableClick" @mousemove="handleMouseMove" class="el-month-table">
+  <table @click="handleMonthTableClick" 
+  class="el-month-table">
     <tbody>
     <tr v-for="(row, key) in rows" :key="key">
       <td :class="getCellStyle(cell)" v-for="(cell, key) in row" :key="key">
