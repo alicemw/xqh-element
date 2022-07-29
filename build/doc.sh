a4d1c094300f7c9aee23f4d2d2ccc0216a3cbcd9
alicew <1660523599@qq.com>
2022-07-29 11:54:25 +0800

    脚本更新

---
+++ build/doc.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+echo "deploy start!"
+
+VERSION=`npx select-version-cli`
+echo "$VERSION"
+# copy
+cp -R ./examples/xqh-element/. ../xqh-doc
+
+cd ../xqh-doc
+# commit
+  git add -A
+  git commit -m "[版本更新] $VERSION"
+  git push
+echo "deploy end!"
